61fbf007cafc2243c0ce8b520df41e21322d174c
Sala de Sistemas 1-6 / Sede 1 <sala161@CIACADEMICO.LOCAL>
2024-11-21 11:34:32 -0500

    Actualizado

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const usuario = {
     nombre: '',
     salario: 0,
     codeudor: false,
+    cuentaCorriente: false,
     fechaNacimiento: '',
     edad: 0
 }
@@ -20,6 +21,7 @@ let prestamo;
 
 document.getElementById('fromUser').addEventListener('submit', function(event) {
     event.preventDefault(); 
+    
     usuario.nombre = document.getElementById('nombreUsuario').value; 
     usuario.salario = document.getElementById('salarioActual').value;
     usuario.codeudor = document.getElementById('coDeudor').value === 'true';
@@ -46,6 +48,14 @@ document.getElementById('fromUser').addEventListener('submit', function(event) {
     if (usuario.edad >= 18) {
         document.getElementById('main__auth').classList.add('ocultar')
         document.getElementById('main__prestamos').classList.remove('ocultar')
+
+        if (usuario.salario >= 900000 && usuario.edad >= 18 && usuario.edad <= 25 && usuario.codeudor == true) {
+            document.getElementById('tresMillones').classList.remove('card__button--block');
+            document.getElementById('tresMillones').innerText = "Solicitar Préstamo";
+        } else {
+            document.getElementById('tresMillones').innerText = "No cumple los requisitos";
+        }
+
     } else {
         alert('Para realizar prestamos debe ser mayor de edad.')
     }
@@ -69,6 +79,18 @@ function prestamoTresMillones(sueldoTrabajador, edad, codeudorFincaRaiz) {
     }
 }
 
-prestamoTresMillones(900000, 24, true);
+function prestamoSeisMillones(sueldoTrabajador, edad, codeudorFincaRaiz, cuentaCorriente) {
+    if (sueldoTrabajador >= 1900000 && edad >= 26 && edad <= 40 && codeudorFincaRaiz && cuentaCorriente) {
+        prestamo = 6000000;
+        descuentoImpuesto = calcularDescuento(prestamo, 0.02, 0.05);
+        saldoActual = prestamo - descuentoImpuesto;
 
+        console.log(`El prestamo se realizó con éxito, su saldo actual es de: ${saldoActual.toFixed(2)} ¡Gracias por confiar en nosotros!`);
+    } else {
+        console.log('Usted no cumple con los requisitos para solicitar este prestamo.');
+    }
+}
+
+prestamoTresMillones(900000, 24, true);
+prestamoSeisMillones(1900000, 26, true, true);
 
