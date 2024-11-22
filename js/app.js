e339e30787ec9d8e462da31364be6f6c1253e7b5
Jose Armando <josearmandodiazhernandez1@gmail.com>
2024-11-21 22:41:54 -0500

    Recibos inicializados

--- js/app.js
+++ js/app.js
@@ -1,20 +1,19 @@
-// Perfil Usuario
-
 const usuario = {
+    identificacion: 0,
     nombre: '',
     salario: 0,
-    codeudor: false,
+    saldo: 0,
+    coDeudor: false,
     cuentaCorriente: false,
     fechaNacimiento: '',
     edad: 0
 }
 
-// 
-
 const salarioMinimo = 1300000;
-let descuentoImpuesto;
-let saldoActual;
-let prestamo;
+let descuentoImpuesto = 0;
+let saldoActual = 0;
+let prestamo = 0;
+
 
 
 // TO-DO: Solicitar Identificación, fecha de nacimiento, dirección y télefono
@@ -22,44 +21,66 @@ let prestamo;
 document.getElementById('fromUser').addEventListener('submit', function(event) {
     event.preventDefault(); 
     
-    usuario.nombre = document.getElementById('nombreUsuario').value; 
-    usuario.salario = document.getElementById('salarioActual').value;
-    usuario.codeudor = document.getElementById('coDeudor').value === 'true';
+    usuario.identificacion = document.getElementById('identificacion').value;
+    usuario.nombre = document.getElementById('nombre').value; 
+    usuario.salario = document.getElementById('salario').value;
+    usuario.coDeudor = document.getElementById('coDeudor').value === 'true';
+    usuario.cuentaCorriente = document.getElementById('cuentaCorriente').value === 'true';
     usuario.fechaNacimiento = document.getElementById('fechaNacimiento').value;
- 
+    const fechaNacimiento = new Date(usuario.fechaNacimiento);
+    const botonTresMillones =  document.getElementById('tresMillones');
+    const botonSeisMillones = document.getElementById('seisMillones')
+    const seccionFormulario = document.getElementById('main__auth');
+    const seccionPrestamos = document.getElementById('main__prestamos');
+    const saldo = document.getElementById('saldo');
+
+    console.log(`La identificación del usuario es: ${usuario.identificacion}`);
     console.log(`El nombre del usuario es: ${usuario.nombre}`); 
     console.log(`El salario del usuario es: ${usuario.salario}`); 
-    console.log(`¿Es codeudor de finca raiz?: ${usuario.codeudor}`);
+    console.log(`¿Es codeudor de finca raiz?: ${usuario.coDeudor}`);
     console.log(`Fecha Nacimiento: ${usuario.fechaNacimiento}`);
-
-    const fechaNacimiento = new Date(usuario.fechaNacimiento);
 
     function calcularEdad(fechaNacimiento) {
         const fechaActual = new Date();
         const añoActual = fechaActual.getFullYear();
         const añoNacimiento = fechaNacimiento.getFullYear();
-        usuario.edad = añoActual - añoNacimiento;
-
-        return usuario.edad;
+        return añoActual - añoNacimiento;
     }
-    
-    calcularEdad(fechaNacimiento);
+    usuario.edad = calcularEdad(fechaNacimiento);
 
     if (usuario.edad >= 18) {
-        document.getElementById('main__auth').classList.add('ocultar')
-        document.getElementById('main__prestamos').classList.remove('ocultar')
+        seccionFormulario.classList.add('ocultar')
+        seccionPrestamos.classList.remove('ocultar')
+        botonTresMillones.innerText = 'No cumple los requisitos'
+        botonSeisMillones.innerText = 'No cumple los requisitos'
 
-        if (usuario.salario >= 900000 && usuario.edad >= 18 && usuario.edad <= 25 && usuario.codeudor == true) {
-            document.getElementById('tresMillones').classList.remove('card__button--block');
-            document.getElementById('tresMillones').innerText = "Solicitar Préstamo";
-        } else {
-            document.getElementById('tresMillones').innerText = "No cumple los requisitos";
+        switch(true) {
+            case (usuario.salario >= 900000 && usuario.edad >= 18 && usuario.edad <= 25 && usuario.coDeudor):
+                botonTresMillones.classList.remove('card__button--block');
+                botonTresMillones.innerText = 'Solicitar Préstamo';
+                botonTresMillones.disabled = false;
+
+                botonTresMillones.addEventListener('click', function(event) {
+                    prestamoTresMillones(usuario.salario, usuario.edad, usuario.coDeudor);
+                });
+
+                break;
+
+            case (usuario.salario >= 1900000 && usuario.edad >= 26 && usuario.edad <= 40 && usuario.coDeudor && usuario.cuentaCorriente):
+                botonSeisMillones.classList.remove('card__button--block');
+                botonSeisMillones.innerText = 'Solicitar Préstamo';
+                break;
+
+            default: 
+
         }
 
     } else {
         alert('Para realizar prestamos debe ser mayor de edad.')
     }
 });
+
+
 
 // Prestamos 
 
@@ -71,19 +92,21 @@ function prestamoTresMillones(sueldoTrabajador, edad, codeudorFincaRaiz) {
     if (sueldoTrabajador >= 900000 && edad >= 18 && edad <= 25 && codeudorFincaRaiz) {
         prestamo = 3000000; 
         descuentoImpuesto = calcularDescuento(prestamo, 0.01, 0.05);
-        saldoActual = prestamo - descuentoImpuesto;
-
+        saldoActual += (prestamo - descuentoImpuesto);
+        saldo.innerText = saldoActual;
         console.log(`El prestamo se realizó con éxito, su saldo actual es de: ${saldoActual.toFixed(2)} ¡Gracias por confiar en nosotros!`);
     } else {
         console.log('Usted no cumple con los requisitos para solicitar este prestamo.');
     }
 }
+
+
 
 function prestamoSeisMillones(sueldoTrabajador, edad, codeudorFincaRaiz, cuentaCorriente) {
     if (sueldoTrabajador >= 1900000 && edad >= 26 && edad <= 40 && codeudorFincaRaiz && cuentaCorriente) {
         prestamo = 6000000;
         descuentoImpuesto = calcularDescuento(prestamo, 0.02, 0.05);
-        saldoActual = prestamo - descuentoImpuesto;
+        saldoActual = (prestamo - descuentoImpuesto);
 
         console.log(`El prestamo se realizó con éxito, su saldo actual es de: ${saldoActual.toFixed(2)} ¡Gracias por confiar en nosotros!`);
     } else {
@@ -91,6 +114,4 @@ function prestamoSeisMillones(sueldoTrabajador, edad, codeudorFincaRaiz, cuentaC
     }
 }
 
-prestamoTresMillones(900000, 24, true);
-prestamoSeisMillones(1900000, 26, true, true);
 
